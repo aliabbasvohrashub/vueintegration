6162afd64f52d0bf00f7020ab6427945e8b6e1ff
AliabbasVohra <aliabbasvohra@gmail.com>
2020-08-09 16:05:34 +0530

    code first enabled

--- WebApplication1/Repository/DatabaseContext.cs
+++ WebApplication1/Repository/DatabaseContext.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using WebApplication1.Models;
 
@@ -9,11 +12,18 @@ namespace WebApplication1.Repository
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext() : base("DbString")
-        {
+        //public DatabaseContext() : base("DbString")
+        //{
 
+        //}
+        protected override void OnModelCreating(DbModelBuilder dbModelBuilder)
+        {
+            dbModelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
         public DbSet<MyFirstTable> MyFirstTable { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
     }
 }
